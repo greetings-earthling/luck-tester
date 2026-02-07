d52a8133035ddf1b7012c9314e44bfc6bf03e783
greetings-earthling <houledennis@gmail.com>
2026-02-07 09:11:38 -0500

    Create fortunelist.js

---
+++ fortunelist.js
@@ -0,0 +1,12 @@
+window.FORTUNES = [
+  "A small win counts. Treat it like one.",
+  "You are closer than you think.",
+  "Say yes to the thing you keep postponing.",
+  "A weird coincidence is still a clue.",
+  "Keep it simple. That’s where your luck lives today.",
+  "You will find the right moment by moving.",
+  "Do one brave thing. Tiny is fine.",
+  "Your future self is quietly cheering.",
+  "Good news loves a calm mind.",
+  "Something easy is about to get easier.",
+];
